e6eb1b278987833ce00a65dafc46bc71d2bad89e
radonious <67727902+radonious@users.noreply.github.com>
2022-10-27 03:24:40 +0700

    Add files via upload

---
+++ lab3/includes.h
@@ -0,0 +1,17 @@
+// System headers
+#include <iostream>
+using namespace std;
+//#include <exception>
+#include <fstream>
+#include <vector>
+#include <sys/time.h>
+
+// Created headers
+#include "MyException/MyException.h"
+
+#include "Time/Time.h"
+#include "Time/Date.h"
+
+#include "Logger/Logger.h"
+#include "Logger/LoggerConsole.h"
+#include "Logger/LoggerFile.h"
---
+++ lab3/main.cpp
@@ -0,0 +1,11 @@
+#include "includes.h"
+
+int main() {
+    Time A;
+    A.subTimeByMs(-20000);
+
+    Time B;
+    B.addTimeByMs(-200);
+    // Return
+    return 0;
+}
